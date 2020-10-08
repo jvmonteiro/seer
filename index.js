8ee0af7e27f0771c9af10e05b42b2ba692238da2
Joao Vitor Monteiro <jvmonteiro@ibm.com>
2020-10-08 07:32:10 -0300

    fix: centers page ui to improve whitespace usage

--- index.js
+++ index.js
@@ -31,29 +31,27 @@ export async function scatter() {
   let plot2 = new Scatterplot(c2);
   let data2 = await plot.loadJSON('./data/superstore.json');
 
+  let low, high, low2, high2, data1, data3;
+
   setInterval(() => {
-    let low, high;
     low = Math.floor(Math.random() * 100);
     high = 200 + Math.floor(Math.random() * 200);
 
-    let data1 = data.slice(low, high);
-    const circles1 = plot.format(data1, { cx: 'Sales', cy: 'Profit', r: 4 });
+    data1 = data.slice(low, high);
+    plot.format(data1, { cx: 'Sales', cy: 'Profit', r: 4 });
     plot.createScales();
     plot.render();
-    console.log('updated plot 1', low, high);
-  }, 1300);
+  }, 1000);
 
   setInterval(() => {
-    let low, high;
-    low = 200 + Math.floor(Math.random() * 150);
-    high = 400 + Math.floor(Math.random() * 100);
+    low2 = 200 + Math.floor(Math.random() * 150);
+    high2 = 400 + Math.floor(Math.random() * 100);
 
-    let data3 = data2.slice(low, high);
-    const circles1 = plot2.format(data3, { cx: 'Sales', cy: 'Profit', r: 6 });
+    data3 = data2.slice(low2, high2);
+    plot2.format(data3, { cx: 'Sales', cy: 'Profit', r: 6 });
     plot2.createScales();
     plot2.render();
-    console.log('updated plot 2', low, high);
-  }, 2300);
+  }, 2000);
 }
 
 scatter();
